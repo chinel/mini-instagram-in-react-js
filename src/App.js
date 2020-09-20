525bbaac31c1a44facc006de1988b254fd7f281e
nelo <nelokwaz@gmail.com>
2020-09-20 22:48:15 +0100

    created the header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome onboard</h1>
-
+    <div className="app">
       {/**Header */}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
       {/**Posts */}
       {/**Post */}
     </div>
