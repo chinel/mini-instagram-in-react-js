e3b602ba386f77a664581664b0df00d195290e7e
nelo <nelokwaz@gmail.com>
2020-09-21 22:55:33 +0100

    created a post component to hold the posts

---
+++ src/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Posts() {
+  return (
+    <div>
+      {/**Header  => avatar and username */}
+      {/**Image  */}
+      {/**Username and caption */}
+    </div>
+  );
+}
+
+export default Posts;
