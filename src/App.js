8035892a48c1b73494d5c84d6305cca8422d7db2
nelo <nelokwaz@gmail.com>
2020-10-09 20:11:24 +0100

    created the structure for the post page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Posts from "./Posts";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           alt=""
         />
       </div>
+      <Posts />
       {/**Posts */}
       {/**Post */}
     </div>
--- src/Posts.js
+++ src/Posts.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import "./Posts.css";
+import Avatar from "@material-ui/core/Avatar";
 function Posts() {
   return (
-    <div>
-      {/**Header  => avatar and username */}
-      {/**Image  */}
-      {/**Username and caption */}
+    <div className="post">
+      <h3>Username</h3>
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+      />
+      <h4 className="post__text">
+        <strong>Username</strong> Caption
+      </h4>
     </div>
   );
 }
