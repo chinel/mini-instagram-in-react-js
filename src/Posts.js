209cf5e011962e6f9af465b78fddf8b92eddc762
nelo <nelokwaz@gmail.com>
2020-10-10 19:37:18 +0100

    added the post header and styled the post block

--- src/Posts.js
+++ src/Posts.js
@@ -4,8 +4,15 @@ import Avatar from "@material-ui/core/Avatar";
 function Posts() {
   return (
     <div className="post">
-      <Avatar />
-      <h3>Username</h3>
+      <div className="post__header">
+        <Avatar
+          className="post__headerAvatar"
+          alt="TestUserName"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>Username</h3>
+      </div>
+
       <img
         className="post__image"
         src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
