5cd8eab3f0fd5004e34a680a5252acce4f928c0d
nelo <nelokwaz@gmail.com>
2020-10-13 18:37:20 -1200

    added register form with inputs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,40 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase";
 import Posts from "./Posts";
+import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
+import { Button } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [posts, setPost] = useState([]);
-  const [close, setClose] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = React.useState(getModalStyle);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -21,10 +50,20 @@ function App() {
 
   return (
     <div className="app">
-      <Modal open={open} onClose={() => setClose(true)}>
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am a body</h2>
-          <SimpleModal />
+          <div className="app_modalHeader">
+            <img
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+            />
+          </div>
+          <form className="app_signup">
+            <input type="email" />
+            <input type="password" />
+            <button>Sign up</button>
+          </form>
         </div>
       </Modal>
       <div className="app__header">
@@ -34,6 +73,7 @@ function App() {
           alt=""
         />
       </div>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       {posts.map(({ id, post }) => (
         <Posts
           key={id}
