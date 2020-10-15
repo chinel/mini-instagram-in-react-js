c4883ad0eed181a9f287f1a446add470ab87ef73
nelo <nelokwaz@gmail.com>
2020-10-14 22:25:17 -1200

    added a basic structure for the image upload component

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 
-const ImageUpload = () => {};
+const ImageUpload = () => {
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter caption...."
+        onChange={(e) => setCaption(e.target.value)}
+      />
+      <input type="file" onChange={handleFileChange} />
+      <Button className="imageUpload__button" onClick={handleUpload}>
+        Upload
+      </Button>
+    </div>
+  );
+};
 
 export default ImageUpload;
